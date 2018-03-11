ea9c2b1446c1a2e69d128a0c5fb25d5fa801583e
leo766 <1779549200@qq.com>
2018-03-11 10:20:15 +0800

    项目初始化配置

---
+++ src/main/java/com/miaomiao/dao/OrderMapper.java
@@ -0,0 +1,17 @@
+package com.miaomiao.dao;
+
+import com.miaomiao.pojo.Order;
+
+public interface OrderMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Order record);
+
+    int insertSelective(Order record);
+
+    Order selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Order record);
+
+    int updateByPrimaryKey(Order record);
+}
---
+++ src/main/java/com/miaomiao/dao/UserMapper.java
@@ -0,0 +1,17 @@
+package com.miaomiao.dao;
+
+import com.miaomiao.pojo.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
